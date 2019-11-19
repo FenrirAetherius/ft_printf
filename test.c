7e6d3a19d9471018e48897f5acf40d01f97b517a
FenrirAetherius <blueyesmax69@gmail.com>
2019-11-19 13:36:39 +0100

    function for %ls are ready and tested

--- test.c
+++ test.c
@@ -3,12 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <wchar.h>
+#include <unistd.h>
 
 int main ()
 {
 	int *n;
 
-	n = malloc(sizeof(int));
+	if(!(n = malloc(sizeof(int))))
+		return(-1);
 
 	printf("test '.f at the same time:\n\n");
 
@@ -47,11 +49,46 @@ int main ()
 	setlocale(LC_CTYPE, "en_US.UTF-8");
 	wchar_t *str;
 
-	str = malloc(sizeof(wchar_t) * 4);
+	if(!(str = malloc(sizeof(wchar_t) * 6)))
+		return(-1);
 	str[0] = 7424;
 	str[1] = 7425;
 	str[2] = 7426;
 	str[3] = 1792;
-	printf("%ls\n", str);
+	str[4] = 0x11a2d;
+	str[5] = 0;
+	printf("%ls%d\n", str, 5);
+
+	char *str2;
+	if(!(str2 = malloc(sizeof(char) * 13)))
+		return(-1);
+	/*\u1d00*/
+	str2[0] = 0xe1;
+	str2[1] = 0xb4;
+	str2[2] = 0x80;
+	/*\u1d01*/
+	str2[3] = 0xe1;
+	str2[4] = 0xb4;
+	str2[5] = 0x81;
+	/*\u1d02*/
+	str2[6] = 0xe1;
+	str2[7] = 0xb4;
+	str2[8] = 0x82;
+	/*\u0700*/
+	str2[9] = 0xdc;
+	str2[10] = 0x80;
+
+	str2[11] = '\n';
+
+	str2[12] = '\0';
+
+	write(1, str2, 13);
+	/*printf("\n%#x\n", (char)str2[0]);
+	printf("\n%#x\n", str2[1]);
+	printf("\n%#x\n", str2[2]);
+	printf("\n%#x\n", str2[3]);
+	printf("\n%#x\n", 'a');
+
+	write(1, "\u1d00\u1d01\u1d02\u0700\n", 13);*/
 	return 0;
 }
